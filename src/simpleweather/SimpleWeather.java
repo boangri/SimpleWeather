e494cf8f5fd57dbfd0b84fd3c90ba0c0af5ecebb
boris@acer <xinu@yandex.ru>
2018-08-26 17:16:44 +0300

    v2.1.6 Added 2 temp sensors, fixed error in WindSpeedSensor

--- src/simpleweather/SimpleWeather.java
+++ src/simpleweather/SimpleWeather.java
@@ -1,14 +1,13 @@
-/******************************************************************************
- 
- Project Name: SimpleWeather
- File name:    SimpleWeather.java
- Version:      1.0   12/17/05
- $Id: SimpleWeather.java,v 1.4 2010/02/28 09:58:54 boris Exp $
- Copyright (C) 2005 by T. Bitson - All rights reserved.
- 
- 
- *****************************************************************************/
-
+/**
+ * ****************************************************************************
+ *
+ * Project Name: SimpleWeather File name: SimpleWeather.java Version: 1.0
+ * 12/17/05 $Id: SimpleWeather.java,v 1.4 2010/02/28 09:58:54 boris Exp $
+ * Copyright (C) 2005 by T. Bitson - All rights reserved.
+ *
+ *
+ ****************************************************************************
+ */
 package simpleweather;
 
 import simpleweather.sensor.*;
@@ -18,255 +17,239 @@ import java.io.*;
 import com.dalsemi.onewire.*;
 import com.dalsemi.onewire.adapter.*;
 
-public class SimpleWeather
-{
-  public static final String VERSION = "SimpleWeather 2.1.5 25.08.2018";
-  private static String MEASUREMENT_INTERVAL; //  Interval between measurements in seconds. Must divide 60.
-  public static String WWW = "www.xland.ru";
-  public static String URL = "/cgi-bin/meteo_upd";
-  public static String StationID = "main";
-  public static boolean debugFlag = false;
-  public long timestamp;
-  public int measurement;
-  public static Properties ps;
-  public Enumeration sensors;
-  public Vector sensor_vector = new Vector(10, 1);
-  public int secs = 0;
-  public int humidityErrorCnt = 0;
-  public int pressureErrorCnt = 0;
-  public int solarErrorCnt = 0; 
-  private DSPortAdapter adapter;
-  private final Wunderground wu;
-  
-  public SimpleWeather()
-  {
-      wu = new Wunderground(this);
-  }
-  
-  public static void main(String[] args) throws Exception
-  {
-    StationProperties sp = new StationProperties("station.properties");
+public class SimpleWeather {
 
-    ps = sp.getStationProperties();
-    
-    System.out.println("Starting " + VERSION);
-    
-    if (args.length != 0)
-    {
-      if (args[0].equals("-d"))
-      {
-        System.out.println("debug on");
-        debugFlag = true;
-      }
+    public static final String VERSION = "SimpleWeather 2.1.6 26.08.2018";
+    private static String MEASUREMENT_INTERVAL; //  Interval between measurements in seconds. Must divide 60.
+    public static String WWW = "www.xland.ru";
+    public static String URL = "/cgi-bin/meteo_upd";
+    public static String StationID = "main";
+    public static boolean debugFlag = false;
+    public long timestamp;
+    public int measurement;
+    public static Properties ps;
+    public Enumeration sensors;
+    public Vector sensor_vector = new Vector(10, 1);
+    public int secs = 0;
+    public int humidityErrorCnt = 0;
+    public int pressureErrorCnt = 0;
+    public int solarErrorCnt = 0;
+    private DSPortAdapter adapter;
+    private final Wunderground wu;
+
+    public SimpleWeather() {
+        wu = new Wunderground(this);
     }
-    
-    try
-    {
-      // get instances to the primary object
-      
-      SimpleWeather sw   = new SimpleWeather();
-      // call the main program loop
-      sw.init();
-      sw.mainLoop();
-    }
-    catch(Throwable t)
-    {
-      System.out.println("Exception: Main() " + t);
-    }
-    finally
-    {
-      System.out.println("SimpleWeather Stopped");
-      System.exit(0);
-    }
-  }
-  
-  private void init()
-  {
-    String ADAPTER_TYPE = ps.getProperty("ADAPTER_TYPE");
-    String ONE_WIRE_SERIAL_PORT = ps.getProperty("ONE_WIRE_SERIAL_PORT"); 
-      
-    Enumeration pse = ps.keys();
-    while (pse.hasMoreElements()) {
-      String str = (String) pse.nextElement();
-      System.out.println(str+"="+SimpleWeather.ps.getProperty(str));
-    }
-    ISensor s;
-    String ID;
-    
-    // get the 1-wire adapter
-    try
-    {
-      // get an instance of the 1-Wire adapter
-      adapter = OneWireAccessProvider.getAdapter(ADAPTER_TYPE, ONE_WIRE_SERIAL_PORT);
-      if (adapter != null)
-      {
-        System.out.println("Found Adapter: " + adapter.getAdapterName() +
-                " on Port " + adapter.getPortName());
-      }
-      else
-      {
-        System.out.println("Error: Unable to find 1-Wire adapter!");
-        System.exit(1);
-      }
-      
-      // reset the 1-Wire bus
-      resetBus();
-    }
-    catch (OneWireException e)
-    {
-      System.out.println("Error Finding Adapter: "+ e);
-      System.exit(1);
-    }
-    
-    ID = ps.getProperty("TEMP_SENSOR1_ID"); // = "A00008001B35DE10"; //WS-1  
-    if (ID != null) {
-        s = new TempSensor(adapter, ID, "temp1");
-        sensor_vector.addElement(s);
-    }
-    ID = ps.getProperty("TEMP_SENSOR2_ID"); // = "A00008001B35DE10"; //WS-1  
-    if (ID != null) {
-        s = new TempSensor(adapter, ID, "temp2");
-        sensor_vector.addElement(s);
-    }
-    ID = ps.getProperty("TEMP_SENSOR3_ID"); // = "A00008001B35DE10"; //WS-1  
-    if (ID != null) {
-        s = new TempSensor(adapter, ID, "temp3");
-        sensor_vector.addElement(s);
-    }
-    ID = ps.getProperty("TEMP_SENSOR4_ID"); // = "A00008001B35DE10"; //WS-1  
-    if (ID != null) {
-        s = new TempSensor(adapter, ID, "temp4");
-        sensor_vector.addElement(s);
-    }
-    ID = ps.getProperty("TEMP1_ID");
-    if (ID != null) {
-        s = new TempSensor(adapter, ID, "temp21");
-        sensor_vector.addElement(s);
-    }
-    ID = ps.getProperty("TEMP2_ID");
-    if (ID != null) {
-        s = new TempSensor(adapter, ID, "temp22");
-        sensor_vector.addElement(s);
-    }
-    ID = ps.getProperty("HUMIDITY_SENSOR_ID");
-    if (ID != null) {
-        s = new HumiditySensor(adapter, ID, ps);
-        sensor_vector.addElement(s);
-    }
-    ID = ps.getProperty("WIND_SPD_ID"); // = "1900000000F7C61D";
-    if (ID != null) {
-        s = new WindSpeedSensor(adapter, ID, ps);
-        sensor_vector.addElement(s);
-    }
-    ID = ps.getProperty("WIND_DIR_ID"); // = "D600000007293320";
-    if (ID != null) {
-        s = new WindDirSensor(adapter, ID, ps);
-        sensor_vector.addElement(s);
-    }
-    ID = ps.getProperty("BARO_SENSOR_ID");
-    if (ID != null) {
-        s = new BaroSensor(adapter, ID, ps);
-        sensor_vector.addElement(s);
-    }
-    ID = ps.getProperty("RAIN_COUNTER_ID");
-    if (ID != null) {
-        s = new RainSensor(adapter, ID, ps);
-        sensor_vector.addElement(s);
-    }
-   
-    MEASUREMENT_INTERVAL = ps.getProperty("MEASUREMENT_INTERVAL");
-    WWW = ps.getProperty("WWW");
-    URL = ps.getProperty("URL");
-    
-    StationID = ps.getProperty("StationID");
-    
-    measurement = 0;
-  }
-  
-  public void mainLoop() throws NumberFormatException
-  {
-    Date date = new Date();
-    int second;
-    int minute, lastMinute = -99;
-    int hour;
-    int interval = Integer.parseInt(MEASUREMENT_INTERVAL);
-    boolean quit = false;
-    InputStreamReader in = new InputStreamReader(System.in);
-    
-    // main program loop
-    while(!quit)
-    {
-      // sleep for 1 second
-      try
-      {
-        Thread.sleep(1000);
-      }
-      catch (InterruptedException e)
-      {}
-      
-      // check current time
-      timestamp = System.currentTimeMillis() / 1000;
-      date.setTime(System.currentTimeMillis());
-      second = date.getSeconds();
-      minute = date.getMinutes();
-      hour = date.getHours();
-      
-      // only loop once a minute
-      //if (minute != lastMinute)
-      if ((second % interval) == 0)
-      {
-        System.out.println("");
-        System.out.println("Time = " + date);
-	measurement += 1;
-        System.out.println("ts = " + timestamp + ", measurement number = "+ measurement + " In q: " + wu.getCount());
-        
-        sensors = sensor_vector.elements();
-        
-        while(sensors.hasMoreElements()) {
-            ISensor s = (ISensor) sensors.nextElement();
-            s.update();
-        } 
-        
-        // update the time
-        lastMinute = minute;
-        
-        if ((minute % 5 == 0) && (second == 0)) {
-            wu.send();
-          
-            sensors = sensor_vector.elements();
-        
-            while(sensors.hasMoreElements()) {
-                ISensor s = (ISensor) sensors.nextElement();
-                s.resetAverage();
-            } 
-            
-	    secs = 0;
+
+    public static void main(String[] args) throws Exception {
+        StationProperties sp = new StationProperties("station.properties");
+
+        ps = sp.getStationProperties();
+
+        System.out.println("Starting " + VERSION);
+
+        if (args.length != 0) {
+            if (args[0].equals("-d")) {
+                System.out.println("debug on");
+                debugFlag = true;
+            }
         }
-      }
-      
-      // development use only - check for 'q' key press
-      try
-      {
-        if (in.ready())
-          if (in.read() == 'q')
-          {
-              quit = true;
 
-              try
-              {
-                adapter.freePort();
-              }
-              catch (OneWireException e)
-              {
-                System.out.println("Error Finding Adapter: "+ e);
-              }
-          }
-      }
-      catch (IOException e)
-      { } // don't care
+        try {
+      // get instances to the primary object
+
+            SimpleWeather sw = new SimpleWeather();
+            // call the main program loop
+            sw.init();
+            sw.mainLoop();
+        } catch (Throwable t) {
+            System.out.println("Exception: Main() " + t);
+        } finally {
+            System.out.println("SimpleWeather Stopped");
+            System.exit(0);
+        }
     }
-  }
-  
+
+    private void init() {
+        String ADAPTER_TYPE = ps.getProperty("ADAPTER_TYPE");
+        String ONE_WIRE_SERIAL_PORT = ps.getProperty("ONE_WIRE_SERIAL_PORT");
+
+        Enumeration pse = ps.keys();
+        while (pse.hasMoreElements()) {
+            String str = (String) pse.nextElement();
+            System.out.println(str + "=" + SimpleWeather.ps.getProperty(str));
+        }
+        ISensor s;
+        String ID;
+
+        // get the 1-wire adapter
+        try {
+            // get an instance of the 1-Wire adapter
+            adapter = OneWireAccessProvider.getAdapter(ADAPTER_TYPE, ONE_WIRE_SERIAL_PORT);
+            if (adapter != null) {
+                System.out.println("Found Adapter: " + adapter.getAdapterName()
+                        + " on Port " + adapter.getPortName());
+            } else {
+                System.out.println("Error: Unable to find 1-Wire adapter!");
+                System.exit(1);
+            }
+
+            // reset the 1-Wire bus
+            resetBus();
+        } catch (OneWireException e) {
+            System.out.println("Error Finding Adapter: " + e);
+            System.exit(1);
+        }
+
+        ID = ps.getProperty("TEMP_SENSOR1_ID"); // = "A00008001B35DE10"; //WS-1  
+        if (ID != null) {
+            s = new TempSensor(adapter, ID, "temp1");
+            sensor_vector.addElement(s);
+        }
+        ID = ps.getProperty("TEMP_SENSOR2_ID"); // = "A00008001B35DE10"; //WS-1  
+        if (ID != null) {
+            s = new TempSensor(adapter, ID, "temp2");
+            sensor_vector.addElement(s);
+        }
+        ID = ps.getProperty("TEMP_SENSOR3_ID"); // = "A00008001B35DE10"; //WS-1  
+        if (ID != null) {
+            s = new TempSensor(adapter, ID, "temp3");
+            sensor_vector.addElement(s);
+        }
+        ID = ps.getProperty("TEMP_SENSOR4_ID"); // = "A00008001B35DE10"; //WS-1  
+        if (ID != null) {
+            s = new TempSensor(adapter, ID, "temp4");
+            sensor_vector.addElement(s);
+        }
+        ID = ps.getProperty("TEMP1_ID");
+        if (ID != null) {
+            s = new TempSensor(adapter, ID, "temp21");
+            sensor_vector.addElement(s);
+        }
+        ID = ps.getProperty("TEMP2_ID");
+        if (ID != null) {
+            s = new TempSensor(adapter, ID, "temp22");
+            sensor_vector.addElement(s);
+        }
+        ID = ps.getProperty("TEMP3_ID");
+        if (ID != null) {
+            s = new TempSensor(adapter, ID, "temp23");
+            sensor_vector.addElement(s);
+        }
+        ID = ps.getProperty("TEMP4_ID");
+        if (ID != null) {
+            s = new TempSensor(adapter, ID, "temp24");
+            sensor_vector.addElement(s);
+        }
+        ID = ps.getProperty("HUMIDITY_SENSOR_ID");
+        if (ID != null) {
+            s = new HumiditySensor(adapter, ID, ps);
+            sensor_vector.addElement(s);
+        }
+        ID = ps.getProperty("WIND_SPD_ID"); // = "1900000000F7C61D";
+        if (ID != null) {
+            s = new WindSpeedSensor(adapter, ID, ps);
+            sensor_vector.addElement(s);
+        }
+        ID = ps.getProperty("WIND_DIR_ID"); // = "D600000007293320";
+        if (ID != null) {
+            s = new WindDirSensor(adapter, ID, ps);
+            sensor_vector.addElement(s);
+        }
+        ID = ps.getProperty("BARO_SENSOR_ID");
+        if (ID != null) {
+            s = new BaroSensor(adapter, ID, ps);
+            sensor_vector.addElement(s);
+        }
+        ID = ps.getProperty("RAIN_COUNTER_ID");
+        if (ID != null) {
+            s = new RainSensor(adapter, ID, ps);
+            sensor_vector.addElement(s);
+        }
+
+        MEASUREMENT_INTERVAL = ps.getProperty("MEASUREMENT_INTERVAL");
+        WWW = ps.getProperty("WWW");
+        URL = ps.getProperty("URL");
+
+        StationID = ps.getProperty("StationID");
+
+        measurement = 0;
+    }
+
+    public void mainLoop() throws NumberFormatException {
+        Date date = new Date();
+        int second;
+        int minute, lastMinute = -99;
+        int hour;
+        int interval = Integer.parseInt(MEASUREMENT_INTERVAL);
+        boolean quit = false;
+        InputStreamReader in = new InputStreamReader(System.in);
+
+        // main program loop
+        while (!quit) {
+            // sleep for 1 second
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+            }
+
+            // check current time
+            timestamp = System.currentTimeMillis() / 1000;
+            date.setTime(System.currentTimeMillis());
+            second = date.getSeconds();
+            minute = date.getMinutes();
+            hour = date.getHours();
+
+      // only loop once a minute
+            //if (minute != lastMinute)
+            if ((second % interval) == 0) {
+                System.out.println("");
+                System.out.println("Time = " + date);
+                measurement += 1;
+                System.out.println("ts = " + timestamp + ", measurement number = " + measurement + " In q: " + wu.getCount());
+
+                sensors = sensor_vector.elements();
+
+                while (sensors.hasMoreElements()) {
+                    ISensor s = (ISensor) sensors.nextElement();
+                    s.update();
+                }
+
+                // update the time
+                lastMinute = minute;
+
+                if ((minute % 5 == 0) && (second == 0)) {
+                    wu.send();
+
+                    sensors = sensor_vector.elements();
+
+                    while (sensors.hasMoreElements()) {
+                        ISensor s = (ISensor) sensors.nextElement();
+                        s.resetAverage();
+                    }
+
+                    secs = 0;
+                }
+            }
+
+            // development use only - check for 'q' key press
+            try {
+                if (in.ready()) {
+                    if (in.read() == 'q') {
+                        quit = true;
+
+                        try {
+                            adapter.freePort();
+                        } catch (OneWireException e) {
+                            System.out.println("Error Finding Adapter: " + e);
+                        }
+                    }
+                }
+            } catch (IOException e) {
+            } // don't care
+        }
+    }
+
     private void resetBus() // reset the 1-wire bus
     {
         System.out.println("Resetting 1-wire bus");
--- src/simpleweather/sensor/WindSpeedSensor.java
+++ src/simpleweather/sensor/WindSpeedSensor.java
@@ -1,126 +1,112 @@
 /******************************************************************************
  
- Project Name: SimpleWeather
- File name:    WindSensor.java
- Version:      1.0.3 02/05/06
- Copyright (C) 2006 by T. Bitson - All rights reserved.
- 
- This class provides the interface to the 1-wire temperature sensor
- device.
- 
- *****************************************************************************/
-
+ * Project Name: SimpleWeather File name: WindSensor.java Version: 1.0.3
+ * 02/05/06 Copyright (C) 2006 by T. Bitson - All rights reserved.
+ *
+ * This class provides the interface to the 1-wire temperature sensor device.
+ *
+ ****************************************************************************
+ */
 package simpleweather.sensor;
 
 import com.dalsemi.onewire.*;
 import com.dalsemi.onewire.adapter.*;
 import com.dalsemi.onewire.container.*;
 import java.util.Properties;
-import simpleweather.SimpleWeather;
-import simpleweather.SimpleWeatherException;
 
-public class WindSpeedSensor extends AbstractSensor
-{
-  // calibration constants
-  private float radius = 0.5f;//  effective radius of the wheel
-  private final String WIND_RADIUS;
-  // class variables
-  private long lastCount = 0;
-  private long lastTicks = 0;
-  private OneWireContainer1D   windSpdDevice = null;
- 
-  public WindSpeedSensor(DSPortAdapter adapter, String windSpdDeviceID, Properties ps)
-  {
-    // get instances of the 1-wire devices
-    windSpdDevice   = new OneWireContainer1D(adapter, windSpdDeviceID);
-    WIND_RADIUS = ps.getProperty("WIND_RADIUS");
-    if (WIND_RADIUS != null) radius = Float.valueOf(WIND_RADIUS);
-    if (windSpdDevice != null)
-    {
-      lastTicks = System.currentTimeMillis();
-      try
-      {
-        lastCount = windSpdDevice.readCounter(15);
-      } 
-      catch (OneWireException e)
-      {
-         System.out.print("Can't create Conatiner20\n");
-      }
-      this.resetAverage();
-    }  
-  }
- 
- 
-  public float getWindSpeed() throws OneWireException
-  {
-    float windSpeed = 0f;
-    
-    
-      if (debugFlag)
-      {
-        System.out.print("Wind Speed: Device = " + windSpdDevice.getName());
-        System.out.print("  ID = " + windSpdDevice.getAddressAsString() + "\n");
-      }
+public class WindSpeedSensor extends AbstractSensor {
 
-      // read wind counter & system time
-      long currentCount = windSpdDevice.readCounter(15);
-      long currentTicks = System.currentTimeMillis();
-      System.out.println("Wind Count: " + currentCount);
+    // calibration constants
+    private float radius = 0.5f;//  effective radius of the wheel
+    private final String WIND_RADIUS;
+    // class variables
+    private long lastCount = 0;
+    private long lastTicks = 0;
+    private OneWireContainer1D windSpdDevice = null;
 
-      if (lastTicks != 0)
-      {
-        // calculate the wind speed based on the revolutions per second
-        //windSpeed = ((currentCount-lastCount)/((currentTicks-lastTicks)/1000f)) / 2.0f * 2.453f;   // MPH
-        //windSpeed = ((currentCount-lastCount)/((currentTicks-lastTicks)/1000f)) / 2.0f * 2.453f * 1609.0f / 3600.0f;   // Met/sec
-        windSpeed = (float)(currentCount-lastCount)/(float)(currentTicks-lastTicks)*1000f*6.28f*radius;
-      }
-
-      if (debugFlag)
-        System.out.println("Count = " + (currentCount-lastCount) + " during " +
-                (currentTicks-lastTicks) + "ms calcs to " + windSpeed + " radius="+radius);
-
-      // remember count & time
-      lastCount = currentCount;
-      lastTicks = currentTicks;
-
-    return windSpeed;
-  }
-  
-  public String getWind()
-  {
-    if (samples == 0) return ("U");
-    double avgWind = Math.sqrt(sumSquares/samples);
-    
-    return this.formatValue((float)avgWind, 1);
-  } 
-  
-  public String getWindPk()
-  {
-    if (samples == 0) return ("U");
-    float avg = sumValues/samples;
-    float disp = sumSquares/samples;
-    double pk = Math.sqrt(disp) + Math.sqrt(disp - avg*avg);
-    
-    return this.formatValue((float)pk, 1);
-  } 
-  
-  public void update()
-  {
-    try {
-        float data = this.getWindSpeed();
-        System.out.println("Wind speed = " + data + " M/sec");
-        updateAverage(data);
-    } catch (OneWireException e) {
-        System.out.println("Error Reading Wind Speed: " + e);
+    public WindSpeedSensor(DSPortAdapter adapter, String windSpdDeviceID, Properties ps) {
+        // get instances of the 1-wire devices
+        windSpdDevice = new OneWireContainer1D(adapter, windSpdDeviceID);
+        WIND_RADIUS = ps.getProperty("WIND_RADIUS");
+        if (WIND_RADIUS != null) {
+            radius = Float.valueOf(WIND_RADIUS);
+        }
+        if (windSpdDevice != null) {
+            lastTicks = System.currentTimeMillis();
+            try {
+                lastCount = windSpdDevice.readCounter(15);
+            } catch (OneWireException e) {
+                System.out.print("Can't create Conatiner20\n");
+            }
+            this.resetAverage();
+        }
     }
-  }
-  
-  public Properties getResults()
-  {
-      Properties p = new Properties();
-      p.setProperty("wspd", getWind());
-      p.setProperty("wspdpk", getWindPk());
-      
-      return p;
-  }
+
+    public float getWindSpeed() throws OneWireException {
+        float windSpeed;
+
+        if (debugFlag) {
+            System.out.print("Wind Speed: Device = " + windSpdDevice.getName());
+            System.out.print("  ID = " + windSpdDevice.getAddressAsString() + "\n");
+        }
+
+        // read wind counter & system time
+        long currentCount = windSpdDevice.readCounter(15);
+        long currentTicks = System.currentTimeMillis();
+        System.out.println("Wind Count: " + currentCount);
+
+        if (((currentCount - lastCount) >= 0) && ((currentTicks - lastTicks) >= 0)) {
+            windSpeed = (float) (currentCount - lastCount) / (float) (currentTicks - lastTicks) * 1000f * 6.28f * radius;
+        } else {
+            throw new OneWireException("Could not get data");
+        }
+
+        if (debugFlag) {
+            System.out.println("Count = " + (currentCount - lastCount) + " during "
+                    + (currentTicks - lastTicks) + "ms calcs to " + windSpeed + " radius=" + radius);
+        }
+
+        lastCount = currentCount;
+        lastTicks = currentTicks;
+
+        return windSpeed;
+    }
+
+    public String getWind() {
+        if (samples == 0) {
+            return ("U");
+        }
+        double avgWind = Math.sqrt(sumSquares / samples);
+
+        return this.formatValue((float) avgWind, 1);
+    }
+
+    public String getWindPk() {
+        if (samples == 0) {
+            return ("U");
+        }
+        float avg = sumValues / samples;
+        float disp = sumSquares / samples;
+        double pk = Math.sqrt(disp) + Math.sqrt(disp - avg * avg);
+
+        return this.formatValue((float) pk, 1);
+    }
+
+    public void update() {
+        try {
+            float data = this.getWindSpeed();
+            System.out.println("Wind speed = " + data + " M/sec");
+            updateAverage(data);
+        } catch (OneWireException e) {
+            System.out.println("Error Reading Wind Speed: " + e);
+        }
+    }
+
+    public Properties getResults() {
+        Properties p = new Properties();
+        p.setProperty("wspd", getWind());
+        p.setProperty("wspdpk", getWindPk());
+
+        return p;
+    }
 }
